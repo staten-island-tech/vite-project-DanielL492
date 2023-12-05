09d5c7709a5a3fe22572e16f40b63c06253075c7
DannyL1234 <144807703+DanielL492@users.noreply.github.com>
2023-12-05 18:22:15 -0500

    Revert "xc"
    This reverts commit e7d19d6488f423caef30be4cc37c92afc81a1bda.

---
+++ vite-project/vite-project/js/dom.js
@@ -0,0 +1,15 @@
+import "../css/style.css";
+//import "../js/main.js"
+
+const DOMSelectors = {
+    title: document.querySelector(".title"),
+    container: document.getElementById("flexcontainer"),
+    everything: document.getElementById("everything"),
+    chicken: document.getElementById("chickenbtn"),
+    shrimp: document.getElementById("shrimpbtn"),
+    vege: document.getElementById("veganbtn"),
+    hot: document.getElementById("hotbtn"),
+    nothot: document.getElementById("nothotbtn"),
+}
+
+export {DOMSelectors}; 
